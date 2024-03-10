7032c6e70ef96e7dced812958321045622def4a8
shimagh74 <shima.ghasemzade92@gmail.com>
2024-03-10 15:57:31 +0330

    add search bar

--- src/components/CountryDropdown.jsx
+++ src/components/CountryDropdown.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { RiMapPinLine, RiArrowDownSLine, RiArrowUpSLine } from 'react-icons/ri';
+// import {Menu} from '@headlessui/react'
+import { HouseContext } from './HouseContext';
 
 const CountryDropdown = () => {
-  return <div>CountryDropdown</div>;
+  const { country, setCountry, countries } = useContext(HouseContext)
+  console.log(countries)
+  const [isOpen, setIsOpen] = useState(false)
+
+  return <div className='relative dropdown'>
+    <button onClick={() => setIsOpen((prev) => !prev)} className='dropdown-btn w-full'>
+      <RiMapPinLine className='dropdown-icon-primary' />
+      <div>
+        <div className='text-[15px] font-medium leading-tight '> {country}</div>
+        <div className='text-[13px]'>Select your place</div>
+      </div>
+      {
+        !isOpen ? (<RiArrowDownSLine className='dropdown-icon-secondary' />) : (<RiArrowUpSLine className='dropdown-icon-secondary' />)
+      }
+    </button>
+    {isOpen && (<div className='dropdown-menu'>
+      {
+        countries.map((country, index) => {
+          return (
+            <div onClick={() => setCountry(country)} key={index}>
+              {country}
+            </div>
+          )
+
+        })
+      }
+    </div>)}
+  </div>;
 };
 
 export default CountryDropdown;
---
+++ src/components/HouseContext.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState, createContext } from 'react';
+
+import { housesData } from '../data'
+
+export const HouseContext = createContext()
+
+
+const HouseContextProvider = ({ children }) => {
+
+  const [houses, setHouses] = useState(housesData)
+  const [country, setCountry] = useState('Location (any)')
+  const [countries, setCountries] = useState([])
+  const [property, setProperty] = useState('Property type (any)')
+  const [properties, setProperties] = useState([])
+  const [price, setPrice] = useState('Price range (any)')
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const allCountries = houses.map((house) => {
+      return house.country
+    });
+    const uniqueCountries = ['Location (any)', ... new Set(allCountries)]
+    setCountries(uniqueCountries)
+  }, [])
+
+  return <HouseContext.Provider value={{
+    country,
+    setCountry,
+    countries,
+    property,
+    setProperty,
+    properties,
+    price,
+    setPrice,
+    houses,
+    loading
+  }}>{children}</HouseContext.Provider>;
+};
+
+export default HouseContextProvider;
